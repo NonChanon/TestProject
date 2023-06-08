14f2ab8b70befe79b451ec5d9f02e88b22862776
Jirakan Koollasing <jirakan.cop@gmail.com>
2023-06-08 19:47:00 +0700

    linked data correction page

--- src/pages/DataResult.tsx
+++ src/pages/DataResult.tsx
@@ -4,70 +4,72 @@ import axios from "axios";
 
 export default function DataResult() {
   
-  const [batches, setBatches] = useState([]);
-  const [lots, setLots] = useState([]);
+  const [datas, setDatas] = useState([]);
+
+  const loadDatas = async() =>{
+    const dataRes = await axios.get("http://localhost:8080/api/lots");
+    setDatas(dataRes.data);
+    console.log(dataRes.data);
+  }
+
+  const grouped = datas.reduce((acc: any, obj: string) => {
+    console.log("Acc ======>", acc)
+    const key: string = obj.batchDate;
+    acc[key] = acc[key] || [];
+    acc[key].push(obj);
+    return acc;
+  }, {});
+
+  const batchDate = Object.keys(grouped);
+  console.log(Object.keys(grouped));
 
   useEffect(() => {
     console.log("Trigger use Effect");
-    loadBatches();
-    loadData();
+    loadDatas();
   }, []);
+  
+  // const dataTable = grouped[].map((lot, i) => {
+  //   return (
+  //     <tbody>
+  //       <tr>
+  //         <td>{i+1}</td>
+  //         <td>{lot.name}</td>
+  //         <td>{lot.totalDoc}</td>
+  //         <td>{lot.batchDate}</td>
+  //         <td>
+  //           <p className={lot.approvalStatus}>{lot.approvalStatus}</p>
+  //         </td>
+  //         <td>{lot.approvedBy}</td>
+  //         <td>{lot.totalDuty}</td>
+  //         <td>{lot.totalDubDutyAmount}</td>
+  //         <td>{lot.totalPayment}</td>
+  //         <td className="action">
+  //           <a href="/detail">
+  //             <svg
+  //               xmlns="http://www.w3.org/2000/svg"
+  //               width="21"
+  //               height="21"
+  //               viewBox="0 0 1024 1024"
+  //             >
+  //               <path
+  //                 fill="#535353"
+  //                 d="M688 312v-48c0-4.4-3.6-8-8-8H296c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h384c4.4 0 8-3.6 8-8zm-392 88c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H296zm376 116c-119.3 0-216 96.7-216 216s96.7 216 216 216s216-96.7 216-216s-96.7-216-216-216zm107.5 323.5C750.8 868.2 712.6 884 672 884s-78.8-15.8-107.5-44.5C535.8 810.8 520 772.6 520 732s15.8-78.8 44.5-107.5C593.2 595.8 631.4 580 672 580s78.8 15.8 107.5 44.5C808.2 653.2 824 691.4 824 732s-15.8 78.8-44.5 107.5zM761 656h-44.3c-2.6 0-5 1.2-6.5 3.3l-63.5 87.8l-23.1-31.9a7.92 7.92 0 0 0-6.5-3.3H573c-6.5 0-10.3 7.4-6.5 12.7l73.8 102.1c3.2 4.4 9.7 4.4 12.9 0l114.2-158c3.9-5.3.1-12.7-6.4-12.7zM440 852H208V148h560v344c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V108c0-17.7-14.3-32-32-32H168c-17.7 0-32 14.3-32 32v784c0 17.7 14.3 32 32 32h272c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z"
+  //               />
+  //             </svg>
+  //           </a>
+  //         </td>
+  //       </tr>
+  //     </tbody>
+  //   );
+  // });
 
-  const loadBatches = async() =>{
-    const batchRes = await axios.get("http://localhost:8080/api/batches");
-    setBatches(batchRes.data);
-    console.log(batchRes.data);
-
-  }
-
-  const loadData = async() =>{
-    const lotRes = await axios.get("http://localhost:8080/api/31_10_2022/lots");
-    setLots(lotRes.data);
-    console.log(lotRes.data);
-  }
-
-  const dataTable = lots.map((lot, i) => {
-    return (
-      <tbody>
-        <tr>
-          <td>{i+1}</td>
-          <td>{lot.name}</td>
-          <td>{lot.totalDoc}</td>
-          <td>{lot.batchDate}</td>
-          <td>
-            <p className={lot.approvalStatus}>{lot.approvalStatus}</p>
-          </td>
-          <td>{lot.approvedBy}</td>
-          <td>{lot.totalDuty}</td>
-          <td>{lot.totalDubDutyAmount}</td>
-          <td>{lot.totalPayment}</td>
-          <td className="action">
-            <a href="/detail">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="21"
-                height="21"
-                viewBox="0 0 1024 1024"
-              >
-                <path
-                  fill="#535353"
-                  d="M688 312v-48c0-4.4-3.6-8-8-8H296c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h384c4.4 0 8-3.6 8-8zm-392 88c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H296zm376 116c-119.3 0-216 96.7-216 216s96.7 216 216 216s216-96.7 216-216s-96.7-216-216-216zm107.5 323.5C750.8 868.2 712.6 884 672 884s-78.8-15.8-107.5-44.5C535.8 810.8 520 772.6 520 732s15.8-78.8 44.5-107.5C593.2 595.8 631.4 580 672 580s78.8 15.8 107.5 44.5C808.2 653.2 824 691.4 824 732s-15.8 78.8-44.5 107.5zM761 656h-44.3c-2.6 0-5 1.2-6.5 3.3l-63.5 87.8l-23.1-31.9a7.92 7.92 0 0 0-6.5-3.3H573c-6.5 0-10.3 7.4-6.5 12.7l73.8 102.1c3.2 4.4 9.7 4.4 12.9 0l114.2-158c3.9-5.3.1-12.7-6.4-12.7zM440 852H208V148h560v344c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V108c0-17.7-14.3-32-32-32H168c-17.7 0-32 14.3-32 32v784c0 17.7 14.3 32 32 32h272c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z"
-                />
-              </svg>
-            </a>
-          </td>
-        </tr>
-      </tbody>
-    );
-  });
-
-  const overviewTable = batches.map((data) => {
+  const overviewTable = batchDate.map((data) => {
     return (
       <div>
         <div className="Batch  shadow row space4 ">
-          <p className="tab">Batch Date : {data.batchDate}</p>
-          <div className="tab line3"></div>
-          <p className="tab">Batch Time : {data.batchTime}</p>
+          <p className="tab">Batch Date : {data}</p>
+          {/* <div className="tab line3"></div>
+          <p className="tab">Batch Time : {data.batchTime}</p> */}
         </div>
         <table className="transaction-table">
           <thead>
@@ -84,7 +86,42 @@ export default function DataResult() {
               <th>Action</th>
             </tr>
           </thead>
-          {dataTable}
+          {
+            grouped[data].map((lot: Array<any>, i: number) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td>{i+1}</td>
+                    <td>{lot.name}</td>
+                    <td>{lot.totalDoc}</td>
+                    <td>{lot.batchDate}</td>
+                    <td>
+                      <p className={lot.approvalStatus}>{lot.approvalStatus}</p>
+                    </td>
+                    <td>{lot.approvedBy}</td>
+                    <td>{lot.totalDuty}</td>
+                    <td>{lot.totalDubDutyAmount}</td>
+                    <td>{lot.totalPayment}</td>
+                    <td className="action">
+                      <a href="/detail">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="21"
+                          height="21"
+                          viewBox="0 0 1024 1024"
+                        >
+                          <path
+                            fill="#535353"
+                            d="M688 312v-48c0-4.4-3.6-8-8-8H296c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h384c4.4 0 8-3.6 8-8zm-392 88c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H296zm376 116c-119.3 0-216 96.7-216 216s96.7 216 216 216s216-96.7 216-216s-96.7-216-216-216zm107.5 323.5C750.8 868.2 712.6 884 672 884s-78.8-15.8-107.5-44.5C535.8 810.8 520 772.6 520 732s15.8-78.8 44.5-107.5C593.2 595.8 631.4 580 672 580s78.8 15.8 107.5 44.5C808.2 653.2 824 691.4 824 732s-15.8 78.8-44.5 107.5zM761 656h-44.3c-2.6 0-5 1.2-6.5 3.3l-63.5 87.8l-23.1-31.9a7.92 7.92 0 0 0-6.5-3.3H573c-6.5 0-10.3 7.4-6.5 12.7l73.8 102.1c3.2 4.4 9.7 4.4 12.9 0l114.2-158c3.9-5.3.1-12.7-6.4-12.7zM440 852H208V148h560v344c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V108c0-17.7-14.3-32-32-32H168c-17.7 0-32 14.3-32 32v784c0 17.7 14.3 32 32 32h272c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z"
+                          />
+                        </svg>
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              );
+            })
+          }
           <tfoot>
             <tr>
               <th className="ltb">Total</th>
