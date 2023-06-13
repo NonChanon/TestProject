55eb8a24584f6f5c20e837f28b2a1b15325af473
Jirakan Koollasing <jirakan.cop@gmail.com>
2023-06-13 17:30:04 +0700

    user and role pages added

---
+++ src/pages/testData.tsx
@@ -0,0 +1,66 @@
+const testData = [
+    {
+        username: "Non",
+        role: "Admin",
+        firstname: "Chanon",
+        lastname: "Gitbun",
+        email: "non2001@gmail.com",
+        createdDate: "02/01/2023",
+        createdUser: "user001",
+        updatedDate: "13/06/2023",
+        updatedUser: "user001",
+        firstLogin: "10/06/2023",
+        lastLogin: "13/06/2023",
+        status: "Active",
+        manage: "Edit"
+
+    },
+    {
+        username: "Cop",
+        role: "Admin",
+        firstname: "Jirakan",
+        lastname: "Koollasing",
+        email: "cop2001@gmail.com",
+        createdDate: "02/01/2023",
+        createdUser: "user002",
+        updatedDate: "13/06/2023",
+        updatedUser: "user001",
+        firstLogin: "10/06/2023",
+        lastLogin: "13/06/2023",
+        status: "Active",
+        manage: "Edit"
+    },
+    {
+        username: "Aom",
+        role: "User",
+        firstname: "Aomm",
+        lastname: "Zupp",
+        email: "Aomzup@gmail.com",
+        createdDate: "02/01/2023",
+        createdUser: "user001",
+        updatedDate: "13/06/2023",
+        updatedUser: "user001",
+        firstLogin: "10/06/2023",
+        lastLogin: "13/06/2023",
+        status: "Inactive",
+        manage: "Edit"
+    },
+    {
+        username: "Test",
+        role: "User",
+        firstname: "tt",
+        lastname: "test",
+        email: "test@gmail.com",
+        createdDate: "02/01/2023",
+        createdUser: "user001",
+        updatedDate: "13/06/2023",
+        updatedUser: "user001",
+        firstLogin: "10/06/2023",
+        lastLogin: "13/06/2023",
+        status: "Active",
+        manage: "Edit"
+    },
+
+];
+
+export default testData
