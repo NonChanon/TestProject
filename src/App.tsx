5c842a33df3c19a6ef8637bf5584e425329e98f7
aomzupp <omsup2544@gmail.com>
2023-06-27 17:41:01 +0700

    pulled backend

--- src/App.tsx
+++ src/App.tsx
@@ -15,24 +15,24 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to={"/lots/all"} />} />
-        <Route path="/lots/all" element={<DataResult />} />
-        <Route path="/lots/approved" element={<DataResult />} />
-        <Route path="/lots/pending" element={<DataResult />} />
-        <Route path="/lots/invaliddata" element={<DataResult />} />
-        <Route path="/lots/denied" element={<DataResult />} />
-        <Route path='/invoice' element={<InvoicePayment />} />
-        <Route path='/detail' element={<DetailCollection />} />
-        <Route path="/:lotname/:customertaxid/edit" element={<EditDetail />} />
-        <Route path='/rd/all' element={<RDTransaction />} />
-        <Route path='/rd/success' element={<RDTransaction />} />
-        <Route path='/rd/fail' element={<RDTransaction />} />
-        <Route path='/reciept' element={<RecieptAS9 />} />
-        <Route path="/lots" element={<Navigate to={"/lots/all"} />} />
-        <Route path="/usermanagement" element={<UserManagement />} />
-        <Route path="/rolemanagement" element={<RoleManagement />} />
-        <Route path="/:lotname" element={<DetailCollection />} />
-      </Routes>
+      <Route path="/" element={<Navigate to={"/lots/all"} />} />
+      <Route path="/lots/all" element={<DataResult />} />
+      <Route path="/lots/approved" element={<DataResult />} />
+      <Route path="/lots/pending" element={<DataResult />} />
+      <Route path="/lots/invaliddata" element={<DataResult />} />
+      <Route path="/lots/denied" element={<DataResult />} />
+      <Route path='/invoice' element={<InvoicePayment />} />
+      <Route path='/detail' element={<DetailCollection />} />
+      <Route path="/:lotname/:customertaxid/edit" element={<EditDetail />} />
+      <Route path='/rd/all' element={<RDTransaction />} />
+      <Route path='/rd/success' element={<RDTransaction />} />
+      <Route path='/rd/fail' element={<RDTransaction />} />
+      <Route path='/reciept' element={<RecieptAS9 />} />
+      <Route path="/lots" element={<DataResult />} />
+      <Route path="/usermanagement" element={<UserManagement />} />
+      <Route path="/rolemanagement" element={<RoleManagement />} />
+      <Route path="/:lotname" element={<DetailCollection />} />
+    </Routes>
     </>
   )
 }
