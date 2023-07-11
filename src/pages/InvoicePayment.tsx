166112df8a9d54e1b4302e4b695e8dcbebf17a44
Nattanicha Sentha <69596340+NattanichaSent@users.noreply.github.com>
2023-07-12 05:57:54 +0700

    update pull img from lot

--- src/pages/InvoicePayment.tsx
+++ src/pages/InvoicePayment.tsx
@@ -24,7 +24,6 @@ interface lotModel {
   paymentStatus: string;
 }
 
-<<<<<<< HEAD
 interface customerModel {
   title: string;
   firstname: string;
@@ -63,31 +62,16 @@ interface contractModel {
   relatedStatus: string;
 }
 
-=======
->>>>>>> 84cd09177f3edf28418bc94bbf2645a9e7f30d0d
 interface dataModel {
   totalItems: number;
   totalPages: number;
   currentPage: number;
-<<<<<<< HEAD
   content: customerModel[];
   lot: lotModel;
-=======
-  content: lotModel[];
-  sumIVStatus: IVStatusModel;
-}
-
-interface IVStatusModel {
-  pending: number;
-  approved: number;
->>>>>>> 84cd09177f3edf28418bc94bbf2645a9e7f30d0d
 }
 
 export default function InvoicePayment() {
-  const [datas, setDatas] = useState<dataModel>({
-    totalItems: 0,
-    totalPages: 0,
-    currentPage: 0,
+  const [datas, setDatas] = useState<any>({
     content: [],
     sumIVStatus: {
       approved: 0,
@@ -120,25 +104,18 @@ export default function InvoicePayment() {
   const [lotName, setLotName] = useState({
     lotNameInput: "",
   });
-  const [tab, setTab] = useState("/invoice/all");
-  const [pageNo, setPageNo] = useState("0");
+  const [tab, setTab] = useState("all");
 
   const path = useLocation().pathname;
   console.log("path = " + path);
   console.log(useLocation());
 
+  let grouped = datas;
+
   const loadDatas = async () => {
     const dataRes = await axios.get(`http://localhost:8080/api/invoice/all`);
     setDatas(dataRes.data);
     console.log("getData is : " + dataRes.data);
-  };
-
-  const loadPageDatas = async (filter: string, pageNo: string) => {
-    const dataRes = await axios.get(
-      `http://localhost:8080/api${filter}?page=${pageNo}`
-    );
-    setDatas(dataRes.data);
-    console.log("currentPage = " + dataRes.data.currentPage);
   };
 
   const loadFilterDatas = async (filter: string) => {
@@ -167,7 +144,6 @@ export default function InvoicePayment() {
     console.log(lotName.lotNameInput);
   };
 
-<<<<<<< HEAD
   console.log(grouped);
 
   if (datas.content != null) {
@@ -230,94 +206,10 @@ export default function InvoicePayment() {
     console.log("asdkasdsaoidj");
   };
 
-=======
->>>>>>> 84cd09177f3edf28418bc94bbf2645a9e7f30d0d
   useEffect(() => {
     console.log("Trigger use Effect");
     loadDatas();
   }, [useLocation().key]);
-
-  function renderPageNumber() {
-    const list = [];
-    if (datas.totalPages > 1) {
-      list.push(
-        <button
-          onClick={() => {
-            setPageNo(`${datas.currentPage == 0 ? 0 : datas.currentPage - 1}`);
-            loadPageDatas(
-              tab,
-              `${datas.currentPage == 0 ? 0 : datas.currentPage - 1}`
-            );
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 2 16 16"
-          >
-            <g transform="translate(24 0) scale(-1 1)">
-              <path
-                fill="none"
-                stroke="#489788"
-                stroke-width="2"
-                d="m9 6l6 6l-6 6"
-              />
-            </g>
-          </svg>
-        </button>
-      );
-      for (let i = 0; i < datas.totalPages; i++) {
-        list.push(
-          <button
-            onClick={() => {
-              setPageNo(`${i}`);
-              loadPageDatas(tab, `${i}`);
-            }}
-            className={pageNo == `${i}` ? `${style.active}` : undefined}
-          >
-            {i + 1}
-          </button>
-        );
-      }
-      list.push(
-        <button
-          onClick={() => {
-            setPageNo(
-              `${
-                datas.currentPage == datas.totalPages - 1
-                  ? datas.totalPages - 1
-                  : datas.currentPage + 1
-              }`
-            );
-            loadPageDatas(
-              tab,
-              `${
-                datas.currentPage == datas.totalPages - 1
-                  ? datas.totalPages - 1
-                  : datas.currentPage + 1
-              }`
-            );
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="7 2 16 16"
-          >
-            <path
-              fill="none"
-              stroke="#489788"
-              stroke-width="2"
-              d="m9 6l6 6l-6 6"
-            />
-          </svg>
-        </button>
-      );
-    }
-    return <div>{list}</div>;
-  }
 
   let sumDoc = 0,
     sumTotalDuty = 0,
@@ -427,13 +319,12 @@ export default function InvoicePayment() {
             <div className={`${style.filter} ${style.spaceTitle2}`}>
               <button
                 className={
-                  tab === "/invoice/all"
+                  tab === "all"
                     ? `${style.filterButtonActive}`
                     : `${style.filterButton}`
                 }
                 onClick={() => {
-                  setTab("/invoice/all");
-                  setPageNo("0");
+                  setTab("all");
                   loadFilterDatas("/invoice/all");
                 }}
               >
@@ -441,13 +332,12 @@ export default function InvoicePayment() {
               </button>
               <button
                 className={
-                  tab === "/invoice/approved"
+                  tab === "approved"
                     ? `${style.filterButtonActive}`
                     : `${style.filterButton}`
                 }
                 onClick={() => {
-                  setTab("/invoice/approved");
-                  setPageNo("0");
+                  setTab("approved");
                   loadFilterDatas("/invoice/approved");
                 }}
               >
@@ -461,13 +351,12 @@ export default function InvoicePayment() {
 
               <button
                 className={
-                  tab === "/invoice/pending"
+                  tab === "pending"
                     ? `${style.filterButtonActive}`
                     : `${style.filterButton}`
                 }
                 onClick={() => {
-                  setTab("/invoice/pending");
-                  setPageNo("0");
+                  setTab("pending");
                   loadFilterDatas("/invoice/pending");
                 }}
               >
@@ -739,17 +628,6 @@ export default function InvoicePayment() {
                       </tr>
                     </tfoot>
                   </table>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                  }}
-                >
-                  <div className={`${style.pagination} ${style.end}`}>
-                    {renderPageNumber()}
-                  </div>
                 </div>
               </div>
             ) : undefined}
